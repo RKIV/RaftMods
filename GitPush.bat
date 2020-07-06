68a14797d1f8f19a1c5eebe736e125549458fa6b
Robert Keller <rkiv@umich.edu>
2020-07-06 18:19:21 -0400

    07/06/2020:18:19:21.91

--- GitPush.bat
+++ GitPush.bat
@@ -1,8 +1,6 @@
 git add .
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
-echo Current time is %mydate%:%mytime%
-pause
 git commit -m "%mydate%:%mytime%"
 git push
 pause
